0e0da4b877f0d6c5d0d545df4e4f0cbae2eff5ee
Matthew Bongiovi <mattbo@microsoft.com>
2017-06-12 15:27:03 -0700

    Finished rough prototype of Seattle911 Cleaner

--- dataLayer/Seattle911Cleaner.py
+++ dataLayer/Seattle911Cleaner.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime, timedelta
+
 from ICleaner import ICleaner
 from HttpRequestManager import HttpRequestManager
 
@@ -7,36 +10,72 @@ class Seattle911Cleaner(ICleaner):
         ICleaner.__init__(self)
         return
 
+    #
+    # This method will return the cleaned data for the Seattle911 data store 
+    # Params: 
+    #    dateRange - the timeframe for which to get data 
+    #    boundingBox - the physical area in which to get the data, specified as a tuple of tuples, as follows:
+    #           boundingBox[0][0] - latitude of upper left corner of bounding box 
+    #           boundingBox[0][1] - longitude of upper left corner of bounding box 
+    #           boundingBox[1][0] - latitude of lower right corner of bounding box 
+    #           boundingBox[1][1] - longitude of lower right corner of bounding box 
+    # Returns: 
+    #    GeoJSON blob containing all data, cleaned, and weighted appropriately 
     def GetData(self, dateRange, boundingBox):
         
-        # boundingBox = ((47.606359, -122.325458), (47.623644, -122.293172))
+        responseGeoJsonTemplate = "{\"type\" : \"FeatureCollection\", \"features\": []}"
+        responseGeoJson = json.loads(responseGeoJsonTemplate)
+        responses = []
         
-        # Query for each 'initial_type_group' as a separate query, and then aggregate them into a single GEOJSON response 
+        # Query for each 'initial_type_group' as a separate query, and then aggregate them into a single GEOJSON response
+        startDate = datetime.today() - timedelta(days=21)
+        endDate = datetime.today() - timedelta(days=7)
+        
+        # TODO: Decide if cleaners should choose date range, or if we should use the date range passed in 
+        
+        whereClause = "at_scene_time between '{4}' and '{5}' AND within_box(incident_location, {0}, {1}, {2}, {3})".format(str(boundingBox[0][0]), str(boundingBox[0][1]), str(boundingBox[1][0]), str(boundingBox[1][1]), startDate.strftime("%Y-%m-%dT%H:%M:%S"), endDate.strftime("%Y-%m-%dT%H:%M:%S"))
+        allFilters = []
+        allFilters.append({ "initial_type_group": "ROAD RAGE", "$where": whereClause })
+        allFilters.append({ "initial_type_group": "TRAFFIC RELATED CALLS", "$where": whereClause })
+        allFilters.append({ "initial_type_group": "ASSAULTS", "$where": whereClause })
+        allFilters.append({ "initial_type_group": "HAZARDS", "$where": whereClause })
+        allFilters.append({ "initial_type_group": "PERSON DOWN/INJURY", "$where": whereClause })
+        
+        for filter in allFilters:
+            responses.append(self.QueryBackend(filter))
 
-        # Get ROAD RAGE events 
-        whereClause = "at_scene_time between '2017-05-05T20:00:00' and '2017-06-07T23:00:00' AND within_box(incident_location, {0}, {1}, {2}, {3})".format(str(boundingBox[0][0]), str(boundingBox[0][1]), str(boundingBox[1][0]), str(boundingBox[1][1]))
-        filters = { "initial_type_group": "ROAD RAGE", "$where": whereClause }
-        self.QueryBackend(filters)
+        # TODO: Handle JSON parsing exceptions  
+        # TODO: Adjust score to not be even across all 911 Call types 
         
-        
-        
-        # Get DUI events 
-        filters = { "initial_type_group": "<ADD DUI MARKER HERE>", "$where": "at_scene_time between '2017-05-15T20:00:00' and '2017-05-24T23:00:00' AND within_box(incident_location, 47.606359, -122.325458, 47.623644, -122.293172)" }
-        
-        
-        return
+        for response in responses:
+            responseObj = json.loads(response)
+            for feature in responseObj['features']: 
+                score = 1
+                responseSchemaTemplate = "{\"type\": \"Feature\", \"geometry\": {}, \"properties\": { \"latitude\": {}, \"longitude\": {}, \"score\": {} }}"
+                cleanFeature = json.loads(responseSchemaTemplate)
+                
+                cleanFeature['geometry'] = feature['geometry']
+                cleanFeature['properties']['latitude'] = feature['properties']['latitude']
+                cleanFeature['properties']['longitude'] = feature['properties']['longitude']
+                cleanFeature['properties']['score'] = str(score)
+                responseGeoJson['features'].append(cleanFeature)
+
+        return responseGeoJson
         
         
     def QueryBackend(self, addedFilters):
                 
         manager = HttpRequestManager("https://data.seattle.gov/resource/pu5n-trf4.geojson")
         
-        #filters = { "initial_type_group": "ROAD RAGE", "$where": "at_scene_time between '2017-05-15T20:00:00' and '2017-05-24T23:00:00'" }
         filters = addedFilters
+        
+        # TODO: Fix headers to work with app token 
+        
+        # TODO: Add paging to our query by default, so that we get all the results (we currently only get 1000)
+        # https://dev.socrata.com/docs/paging.html
         
         addedHeaders = { "X-App-Token": "88CbAdhF0j6N1usyYmBDdtCMI" }
         addedHeaders = {}
         response = manager.post( addedQsParams = filters, addedHeaders = addedHeaders)
-                
-        print(response)
-        return response 
+        
+        return response
--- dataLayer/Seattle911Cleaner_Test.py
+++ dataLayer/Seattle911Cleaner_Test.py
@@ -4,4 +4,6 @@ cleaner = Seattle911Cleaner()
 
 #boundingBox = ((47.606359, -122.325458), (47.623644, -122.293172))
 boundingBox = ((47.636030, -122.365352), (47.578296, -122.288318))
-cleaner.GetData("", boundingBox)
+data = cleaner.GetData("", boundingBox)
+
+print(data)
