39d558dfd1348c5bda982ca700bb0851523cda31
Manish Gupta <manishkg97@gmail.com>
2017-06-11 21:28:45 -0700

    Wrote up basic data aggregator which combines all the lists

--- dataLayer/Main.py
+++ dataLayer/Main.py
@@ -1,4 +1,19 @@
 from Seattle911Cleaner import Seattle911Cleaner
+from DataAggregator import DataAggregator
 
-cleaner = Seattle911Cleaner()
-cleaner.QueryBackend()
+# cleaner = Seattle911Cleaner()
+# cleaner.QueryBackend(filters)
+
+aggregator = DataAggregator()
+
+dateRange = ""
+boundingBox = ((47.606359, -122.325458), (47.623644, -122.293172))
+knobWeights = {
+                "Safety": 1
+              }
+
+aggregator.GetAllCleanData(dateRange, boundingBox, knobWeights)
+
+
+
+
--- dataLayer/Seattle911Cleaner.py
+++ dataLayer/Seattle911Cleaner.py
@@ -1,5 +1,6 @@
 from ICleaner import ICleaner
 from HttpRequestManager import HttpRequestManager
+import json 
 
 class Seattle911Cleaner(ICleaner):  
 
@@ -14,17 +15,19 @@ class Seattle911Cleaner(ICleaner):
         # Query for each 'initial_type_group' as a separate query, and then aggregate them into a single GEOJSON response 
 
         # Get ROAD RAGE events 
-        whereClause = "at_scene_time between '2017-05-05T20:00:00' and '2017-06-07T23:00:00' AND within_box(incident_location, {0}, {1}, {2}, {3})".format(str(boundingBox[0][0]), str(boundingBox[0][1]), str(boundingBox[1][0]), str(boundingBox[1][1]))
-        filters = { "initial_type_group": "ROAD RAGE", "$where": whereClause }
-        self.QueryBackend(filters)
+        # whereClause = "at_scene_time between '2017-05-05T20:00:00' and '2017-06-07T23:00:00' AND within_box(incident_location, {0}, {1}, {2}, {3})".format(str(boundingBox[0][0]), str(boundingBox[0][1]), str(boundingBox[1][0]), str(boundingBox[1][1]))
+        # filters = { "initial_type_group": "ROAD RAGE", "$where": whereClause }
+
+        filters = { "initial_type_group": "ROAD RAGE", "$where": "at_scene_time between '2017-05-15T20:00:00' and '2017-05-24T23:00:00'" }
+        return self.QueryBackend(filters)
         
         
         
         # Get DUI events 
-        filters = { "initial_type_group": "<ADD DUI MARKER HERE>", "$where": "at_scene_time between '2017-05-15T20:00:00' and '2017-05-24T23:00:00' AND within_box(incident_location, 47.606359, -122.325458, 47.623644, -122.293172)" }
+        # filters = { "initial_type_group": "<ADD DUI MARKER HERE>", "$where": "at_scene_time between '2017-05-15T20:00:00' and '2017-05-24T23:00:00' AND within_box(incident_location, 47.606359, -122.325458, 47.623644, -122.293172)" }
         
         
-        return
+        # return
         
         
     def QueryBackend(self, addedFilters):
@@ -37,6 +40,6 @@ class Seattle911Cleaner(ICleaner):
         addedHeaders = { "X-App-Token": "88CbAdhF0j6N1usyYmBDdtCMI" }
         addedHeaders = {}
         response = manager.post( addedQsParams = filters, addedHeaders = addedHeaders)
-                
-        print(response)
-        return response 
+
+        responseDict = json.loads(response)
+        return responseDict
