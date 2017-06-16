5e79977df8821f193d2b5beb8986d02be48a8080
Matthew Bongiovi <mattbo@microsoft.com>
2017-06-16 09:56:36 -0700

    Add XHR request from JS to backend

--- ui/mapDisplay.js
+++ ui/mapDisplay.js
@@ -23,11 +23,40 @@ var draw = new MapboxDraw({
 map.addControl(draw);
 
 var calcButton = document.getElementById('calculate');
+var startPoint;
+var endPoint; 
+
+var sendRequestFunction = function() {
+    var http = new XMLHttpRequest();
+    var url = "http://127.0.0.1:8000/routeCalc/";
+    var knobs = { "Accessibility": 0.5, "Safety": 1, "Nature": 0.2, "Toilets": 0.1 }
+    var params = {"startLatititude" : startPoint[0], "startLongitude": startPoint[1], "endLatititude": endPoint[0], "endLongitude": endPoint[1], "knobWeights": knobs}
+    
+    console.log(params)
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+        }
+    }
+    http.send(params);
+}
+
 calcButton.onclick = function() {
     var data = draw.getAll();
     if (data.features.length > 0) {
-        // TODO: Make backend call with bounding box
+        // Get the first two points drawn on the map 
+        startPoint = data.features[0].geometry.coordinates[0][0];
+        endPoint = data.features[0].geometry.coordinates[0][1];
+        
+        // Make the backend call to the route calc API 
+        sendRequestFunction();
     } else {
         alert("Use the draw tools to draw a polygon!");
     }
 };
+
