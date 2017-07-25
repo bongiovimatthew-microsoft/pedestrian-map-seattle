eb434020bc90956d84438369c3ad311844b0073d
Matthew Bongiovi <bongiovimatthew@gmail.com>
2017-07-24 18:50:04 -0700

    Adding greedy select

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var turf = require('./turf.min.js');
 var url = require('url');
 var express = require('express');
 var bodyParser = require('body-parser');
+var Graph = require("graphlib").Graph;
 
 var app = express();
 
@@ -48,7 +49,7 @@ app.post('/', function(request, response) {
                 console.log("Begin waypoint calculation");
                     
                 // Request contains data, try to use the data 
-                CalculateWaypoints(dataFromPost, response);
+                CalculateWaypoints(postBody, response);
                 proccessedRequest = true;
                 
             } catch(e) {
@@ -68,36 +69,136 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function CalculateWaypoints(dataPoints, res){
+function CreateCostMatrixFromGrid(squareGrid, startPoint, endPoint){
+    var costMatrix = [];
+
+    var col_count = 0;
+    var row_count = 0;
+    var last_lat = 0;
+
+    var startPointFeature = turf.point(startPoint);
+    var endPointFeature = turf.point(endPoint);
+
+    var startCellIndex = [];
+    var endCellIndex = [];
+    
+    for (var i = 0; i < squareGrid.features.length; i++){
+
+        if (i > 0){
+            last_lat = squareGrid.features[i - 1].geometry[0];    
+        }
+        
+        var curr_lat = squareGrid.features[i].geometry[0];    
+        if (curr_lat != last_lat){
+            // We have just moved from the first column to the second 
+            col_count++; 
+            row_count = 0;
+        }else{
+            row_count++;            
+        }
+    
+        // Add the cell score to the current (row, col) entry 
+        costMatrix[row_count][col_count] = [squareGrid.features[i].properties.totalScore, i];
+        if(turf.inside(startPointFeature, squareGrid.features[i])){
+            // Mark the (row, col) index of the start point 
+            startCellIndex = [row_count, col_count];
+        }
+    
+        if(turf.inside(endPointFeature, squareGrid.features[i])){
+            // Mark the (row, col) index of the end point 
+            endCellIndex = [row_count, col_count];
+        }
+
+    }
+
+    var returnData = { "costMatrix": costMatrix, "startCellIndex": startCellIndex, "endCellIndex": endCellIndex };
+
+    return returnData;
+}
+
+function GetGridIndexFromCostMatrix(row, col, costMatrix){
+    return costMatrix[row][col][1];
+}
+
+function isValidIndex(row, col, costMatrix){
+    if(costMatrix[row][col]){
+        return true;
+    }
+
+    return false;
+}
+
+function GreedySelectWaypoints(squareGrid, startPoint, endPoint){
+    // Use Dijkstra's to do shortest path 
+
+    // 1. Build cost matrix from squareGrid
+    var costMatrixData = CreateCostMatrixFromGrid(squareGrid, startPoint, endPoint);
+    var costMatrix = costMatrixData.costMatrix;
+
+    // Create a new directed graph
+    var g = new Graph();
+
+    // Build graph from costMatrix 
+    for(var row = 0; row < costMatrix.length; row++){
+        for(var col = 0; col < costMatrix[row].length; col++){
+
+            var curr_index = GetGridIndexFromCostMatrix(row, col, costMatrix);
+            if(isValidIndex(row + 1, col, costMatrix)){
+                var adjacent_index = GetGridIndexFromCostMatrix(row + 1, col, costMatrix);
+                g.setEdge(curr_index, adjacent_index, { weight: costMatrix[row + 1][col][0]});
+            }
+
+            if(isValidIndex(row - 1, col, costMatrix)){
+                var adjacent_index = GetGridIndexFromCostMatrix(row - 1, col, costMatrix);
+                g.setEdge(curr_index, adjacent_index, { weight: costMatrix[row - 1][col][0]});
+            }
+
+            if(isValidIndex(row, col + 1, costMatrix)){
+                var adjacent_index = GetGridIndexFromCostMatrix(row, col + 1, costMatrix);
+                g.setEdge(curr_index, adjacent_index, { weight: costMatrix[row][col + 1][0]});
+            }
+
+            if(isValidIndex(row, col - 1, costMatrix)){
+                var adjacent_index = GetGridIndexFromCostMatrix(row, col - 1, costMatrix);
+                g.setEdge(curr_index, adjacent_index, { weight: costMatrix[row][col - 1][0]});
+            }            
+        }
+    }
+
+    var shortDistances = graphlib.alg.dijkstra(g, "A", function(e){ return e.weight });  
+
+    var max = 1000000;
+    var i = 0;
+    var pathCellsArray = [];
+    var curr_node = shortDistances[costMatrixData.endCellIndex];
+    while(i < max){
+        i++;
+        if(curr_node === costMatrixData.startCellIndex){
+            break;
+        }
+
+        pathCellsArray.unshift(curr_node);
+
+        curr_node = shortDistances[curr_node.predecessor];
+    }
+
+    console.log("Done doing the shortest path calculations!");
+    console.log(pathCellsArray);
+
+    return pathCellsArray;
+
+}
+
+function CalculateWaypoints(postBody, res){
     console.log("Starting CalculateWaypoints");
     
+    var dataPoints = postBody.data;
     var bbox = turf.bbox(dataPoints);
-
-    var from = {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        // minX, minY 
-        "coordinates": [bbox[0], bbox[1]]
-      }
-    };
-    var to = {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Point",
-        // maxX, minY 
-        "coordinates": [bbox[2], bbox[1]]
-      }
-    };
 
     console.log("Have bounding box, working on square grid");
     
     // var units = 'miles';
     var units = 'feet';
-    // var distance = turf.distance(from, to, units);
-    // var cellSize = distance / 5 
     var cellSize = 300;
     //console.log(cellSize)
     var squareGrid = turf.squareGrid(bbox, cellSize, units);
@@ -124,45 +225,19 @@ function CalculateWaypoints(dataPoints, res){
       return 0;
     }
 
-    console.log("Sorting grid objects");
+    var startPoint = [postBody.startLatitude, postBody.startLongitude];
+    var endPoint = [postBody.endLatitude, postBody.endLongitude];
     
-    // Sort the collected set by polygon area, from smallest to largest
-    var sortedGrid = collected.features.sort(compare); 
-    console.log(sortedGrid) 
+    console.log("About to start GreedySelectWaypoints");
+    var waypoints = GreedySelectWaypoints(collected, startPoint, endPoint);
+    console.log("GreedySelectWaypoints is done");
 
-    // Starting from the start point, walk towards the end point 
-    //  using greedy select 
-
-    // 1. Select cell with source point as curr_cell
-    // 2. Find all adjacent cells to curr_cell and put them in active_cell_list 
-    // 3. Calculate cost of all cells in active_cell_list by: 
-    //         1. Horizontal Cost = (C1 + C2)/2 
-    //         2. Diag Cost = sqrt(2)/2 * (C1 + C2) 
-    // 4. Update cell costs 
-    // 5. Check if destination point is in active_cell_list, if so, stop
-    // 6. Select lowest cost cell as curr_cell, loop to 2
+    // Pick waypoints from path cells 
 
 
 
-    // Take the top five best scores from the sorted grid, and use their centroid as the waypoints
-    waypoints = []
-    waypointObjs = [] 
-    for(i = 0; i < 5; i++){
-        waypoints.push(turf.centroid(sortedGrid[i]));
-        centroid = {
-            "type": "Feature",
-            "properties": {
-                "icon": "rocket"
-            },
-            "geometry": turf.centroid(sortedGrid[i]).geometry
-        };
-        waypointObjs.push(centroid)
-    }
-    //console.log(waypointObjs)
-    var waypointsDisplay = {"type": "FeatureCollection", "features": waypointObjs};
-    //res.write(JSON.stringify(waypointsDisplay)); //write a response to the client
+
     res.write(JSON.stringify(waypoints)); //write a response to the client
-
     console.log("Done!-----------------------");
     console.log(JSON.stringify(waypoints));
 }
