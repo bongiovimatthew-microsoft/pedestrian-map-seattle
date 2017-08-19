8f37cca1c59fb961b9d1514650de85a79b705a92
Matthew Bongiovi <mattbo@microsoft.com>
2017-08-19 12:33:04 -0700

    Clean up costing algorithm, add comments

--- RouteCalc/views.py
+++ RouteCalc/views.py
@@ -8,23 +8,33 @@ import urllib.parse
 import osmnx as ox 
 import networkx as nx
 import math
-
-# issue-manish-06112017 there's prolly a better way to do this 
 import sys
-#sys.path.insert(0, '..\datacleaners')
 from DataCleaners import DataAggregator
 
 GOOGLE_API_KEY = 'AIzaSyDhdh1eXucfnK3EytcFAqTd7rt8-y9N7bw';
 
-
-def wayPointsSortKeyFunc(startPoint, wayPoint1):
-    dist1 = getDistanceBetweenTwoPoints(startPoint, wayPoint1)
-
-    return (dist1)
-
+#
+# Get the Euclidean distance between two poins 
+# 
+# Parameters: 
+#   point1 - a tuple (x, y)
+#   point2 - a tuple (x, y)
+#
+# Returns: 
+#   The Euclidean distance between the points 
+#
 def getDistanceBetweenTwoPoints(point1, point2):
     return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
 
+#
+# Generate a graph of the walking path network for a given bounding box
+#
+# Parameters: 
+#   boundingBox - Two-tuple of points ((x1, y1), (x2, y2))
+#
+# Returns: 
+#   A networkx MultiGraph of the walking path network, generated from OSM
+#
 def getWalkingNetworkGraph(boundingBox):
 
     ox.config(log_file=True, log_console=True, use_cache=True)
@@ -35,16 +45,38 @@ def getWalkingNetworkGraph(boundingBox):
     east = max(boundingBox[0][1], boundingBox[1][1]) # max of lons
     west = min(boundingBox[0][1], boundingBox[1][1]) # min of lons
 
-    G = ox.graph_from_bbox(north=north, south=south, east=east, west=west, network_type='walk')
-    #fig, ax = ox.plot_graph(G)
-    return G
+    return ox.graph_from_bbox(north=north, south=south, east=east, west=west, network_type='walk')
 
+#
+# Generate a least-cost path through a graph containing nodes with lat/long data
+#
+# Parameters: 
+#   graph - a networkx MultiGraph, containing edges with the 'total_cost' attribute 
+#   startLat - the latitude of the starting point for the path 
+#   startLong - the longitude of the starting point for the path 
+#   endLat - the latitude of the ending point for the path 
+#   endLong - the longitude f the ending point for the path 
+#
+# Returns: 
+#   A networkx path (a set of node IDs) for the least-cost path 
+#
 def getLeastCostPath(graph, startLat, startLong, endLat, endLong):
     startNode = getNodeFromLocation(graph, startLat, startLong)
     endNode = getNodeFromLocation(graph, endLat, endLong)
 
     return nx.shortest_path(graph, startNode, endNode, weight='total_cost') # need to specify source and target by node index 
 
+#
+# Generate the cost a given point has on a given edge 
+#
+# Parameters: 
+#   graph - a networkx MultiGraph 
+#   edge - a networkx MultiGraph edge between two nodes
+#   point - a datapoint containing three attribues, 'x', 'y', and 'cost'
+#
+# Returns: 
+#   The cost that should be applied to the given edge based on the given datapoint 
+#
 def edgeCostFromDataPoint(graph, edge, point):
     startNode = graph.node[edge[0]] 
     endNode = graph.node[edge[1]]  
@@ -57,10 +89,25 @@ def edgeCostFromDataPoint(graph, edge, point):
 
     return 0
 
+#
+# Modify the edges of a graph with costs based on the supplied datapoints 
+#
+# Parameters: 
+#   graph - networkx MultiGraph, non-null, non-empty
+#   datapoints - GeoJson dictionary containing x,y geometry coordinates, and a 'score' property
+#
+# Returns: 
+#   graph - networkx MultiGraph, with edges now containing the 'total_cost' attribute 
+#
 def modifyGraphWithCosts(graph, datapoints):
+
+    # Iterate through every edge in the graph, and apply 
+    #  the appropriate point weight to the edge cost 
     for edge in graph.edges_iter(data=True, keys=True):
         total_cost = 0
         
+        # Iterate through every datapoint to determine its 
+        #  weight on the current edge 
         for feature in datapoints['features']: 
             point = {}
             point['x'] = feature['geometry']['coordinates'][1]
@@ -69,25 +116,46 @@ def modifyGraphWithCosts(graph, datapoints):
 
             total_cost += edgeCostFromDataPoint(graph, edge, point)
         
-        # Use the add_edge function with the correct key (edge[2]) to update the edge to include the total_cost attr
+        # Adjust edge costs to be positive (Dijkstra's implementation doesn't want negative)
         if total_cost != 0:
             total_cost *= -1.0
+
+        # Use the add_edge function to update the current edge with the total_cost attribute    
         graph.add_edge(edge[0], edge[1], edge[2], total_cost = total_cost)
 
     return graph
 
-def getNodeFromLocation(graph, lat, long):
-    eps = 0.003
+#
+# Get the node in the graph that is closest to the given lat/long
+#
+# Parameters: 
+#   graph - a networkx MultiGraph, where each node contains attributes 'x' and 'y' 
+#       corresponding to longitude, latitude respectively
+#
+#   lat - the latitude of the point to locate 
+#   lon - the longitude of the point to locate 
+#
+# Returns: 
+#   node ID for the closest node in the graph 
+#
+def getNodeFromLocation(graph, lat, lon):
+    closest_node = 0
+    min_distance = -1
+
     for node, data in graph.nodes_iter(data=True): 
-        if abs(data['x'] - long) < eps:
-            if abs(data['y'] - lat) < eps:
-                return node
-    return -1
+        point1 = [data['x'], data['y']]
+        point2 = [lon, lat]
+        distance = getDistanceBetweenTwoPoints(point1, point2)
+        
+        if min_distance == -1 or distance < min_distance: 
+            min_distance = distance
+            closest_node = node 
+
+    return closest_node
 
 # issue-manigu-06112017 remove csrf exempt
 # issue-manigu-06112017 using post for now, might want to make this a get?
 # todo-manigu-06122017 validate all points are in the request
-# Create your views here.
 @csrf_exempt
 def RouteCalcCore(request):
     print("request.Method: " + request.method)
@@ -113,6 +181,9 @@ def RouteCalcCore(request):
     endLatitude = requestDict['endLatitude']
     endLongitude = requestDict['endLongitude']
 
+    #
+    # Increase the bounding box around the start/end points 
+    #
     if requestDict['startLatitude'] < requestDict['endLatitude']:
         startLatitude -= 0.005
         endLatitude += 0.005
@@ -128,15 +199,10 @@ def RouteCalcCore(request):
         endLongitude -= 0.005
 
     boundingBox = ((startLatitude, startLongitude), (endLatitude, endLongitude))
-    actualcoords = ((requestDict['startLatitude'], requestDict['startLongitude']), (requestDict['endLatitude'], requestDict['endLongitude']))
     knobWeights = requestDict['knobWeights']
-
-    startPointCoord = [requestDict['startLatitude'], requestDict['startLongitude']]
-    endPointCoord = [requestDict['endLatitude'], requestDict['endLongitude']]
 
     allData = aggregator.GetAllCleanData(dateRange, boundingBox, knobWeights)    
 
-    print(boundingBox)
     print(allData)
 
     graph = getWalkingNetworkGraph(boundingBox)
@@ -147,7 +213,8 @@ def RouteCalcCore(request):
         print(edge)
 
     print("Least cost path") 
-    print(getLeastCostPath(graph, startLatitude, startLongitude, endLatitude, endLongitude))
+    path = getLeastCostPath(graph, startLatitude, startLongitude, endLatitude, endLongitude) 
+    print(path)
 
     print("Finished doing all the graph generation stuff")
     
